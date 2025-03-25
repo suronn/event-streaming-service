fccee8238aef0390a9233363d3647ed322d83eb4
Ronnie Horo <ronniehoro@gmail.com>
2025-03-25 09:31:45 +0100

    Initial commit

---
+++ src/main/java/com/streaming/EventStreamingAppApplication.java
@@ -0,0 +1,13 @@
+package com.streaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventStreamingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventStreamingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/streaming/EventStreamingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventStreamingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
